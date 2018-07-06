f098309fafbcf0a25e6b1db5cd7c17deb08f9454
Bjorn Andersson <bjorn.andersson@linaro.org>
2018-07-06 12:02:09 -0700

    util: Guard container_of from duplicate definition
    Add define guards for container_of macro, as it's already defined in
    libqrtr.h.
    
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- util.h
+++ util.h
@@ -38,10 +38,12 @@
 
 void print_hex_dump(const char *prefix, const void *buf, size_t len);
 
+#ifndef container_of
 #define container_of(ptr, type, member) ({ \
 		const typeof(((type *)0)->member)*__mptr = (ptr);  \
 		(type *)((char *)__mptr - offsetof(type, member)); \
 		})
+#endif
 
 #define __packed __attribute__((packed))
 
