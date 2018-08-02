4790df98ce66c0804e668fc3a8cdf83187214cba
Bjorn Andersson <bjorn.andersson@linaro.org>
2018-08-01 23:55:41 -0700

    list: Add guard check for container_of
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- router/list.h
+++ router/list.h
@@ -34,10 +34,12 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#ifndef container_of
 #define container_of(ptr, type, member) ({ \
 		const typeof(((type *)0)->member)*__mptr = (ptr);  \
 		(type *)((char *)__mptr - offsetof(type, member)); \
 		})
+#endif
 
 struct list_head {
 	struct list_head *prev;
